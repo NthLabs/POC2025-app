55338359f8f0f1f00fee135050c619893d3d4f01
NthLabs <nthlabs@nth.com>
2025-03-31 14:02:53 -0700

    Add RAG Demo

---
+++ pages/90_RAG_Demo.py
@@ -0,0 +1,221 @@
+# !pip install streamlit langchain langchain-nvidia-ai-endpoints langchain_community pypdf langchain-chroma
+
+import streamlit as st
+from langchain_nvidia_ai_endpoints import ChatNVIDIA, NVIDIAEmbeddings
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.callbacks import get_openai_callback
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_chroma import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+import os, os.path
+import time
+import nthUtility
+from poc_env import *
+
+# My Variables
+webTitle = "RAG Demo"               # Title on Browser
+myDocs = "./data/RAGdemo"           # Location for PDFs
+vsPath = "./catalog/RAGdemo"        # Location for VectorStore
+logFile = "./logs/RAGprompt.log"    # Location for Prompt Logs
+perfLog = "./logs/RAGperf.log"      # Location for Perf Log
+logo = "images/NthLabs.png"         # 
+msgHistory = "messagesRAG"          # This should be unique for each streamlit page
+vsName = "RAGdemo"                  # This should be unique for each Chroma instance
+
+nthUtility.file_structure(myDocs)   # Setup File structure
+
+
+
+# LLMs and Embeddings
+llm = ChatNVIDIA(
+    base_url=f"http://{llm1Addr}/v1",
+    api_key="FAKE",
+    model=llm1Model,
+    temperature=0.9)
+
+embedding = NVIDIAEmbeddings(
+    base_url=f"http://{embedAddr}/v1",
+    api_key="FAKE",
+    model=embedModel,
+    )
+
+#-------------------------------------------------------------
+# LangChain Workflow:
+# Get VectorStore
+# build context retriever chain (from Vectorstore)
+# build conversational RAG chain (from retriever chain)
+# get response 
+#-------------------------------------------------------------
+
+# LangChain Functions
+def create_vectorstore():
+    loader = PyPDFDirectoryLoader(myDocs)
+    documents = loader.load()
+
+    # Chunk the data
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", ".", ";", ",", " ", ""],
+    )
+    docChunks = text_splitter.split_documents(documents)
+    
+    vectorStore = Chroma.from_documents(
+        documents=docChunks,
+        embedding=embedding,
+        persist_directory=vsPath,
+    )
+    setattr(st.session_state, vsName, get_vectorstore())
+
+
+def get_vectorstore():
+    if not os.path.exists(vsPath):
+        create_vectorstore()
+    vectorStore = Chroma(
+        embedding_function=embedding,
+        persist_directory=vsPath,)
+    return vectorStore
+
+def regen_vectorstore():
+    vectorStore = get_vectorstore()
+    vectorStore.delete_collection()
+    create_vectorstore()
+
+
+def get_context_retriever_chain(vectorStore):
+    retriever = vectorStore.as_retriever(
+        search_kwargs={"k": 2})
+    prompt = ChatPromptTemplate.from_messages([
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+      ("user", """Given the above conversation, generate a search query to 
+       look up in order to get information relevant to the conversation""")
+    ])
+    retrieverChain = create_history_aware_retriever(llm, retriever, prompt)
+    return retrieverChain
+
+
+def get_conversational_rag_chain(retrieverChain): 
+    prompt = ChatPromptTemplate.from_messages([
+      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+      MessagesPlaceholder(variable_name="chat_history"),
+      ("user", "{input}"),
+    ])
+    stuffDocumentsChain = create_stuff_documents_chain(llm,prompt)
+    return create_retrieval_chain(retrieverChain, stuffDocumentsChain)
+
+
+def get_response(userInput):
+    retriever_chain = get_context_retriever_chain(getattr(st.session_state, vsName))
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+    timeStart = time.perf_counter()
+    with get_openai_callback() as cb:
+        response = conversation_rag_chain.invoke({
+            "chat_history": getattr(st.session_state, msgHistory),
+            "input": userInput
+        })
+    timeEnd = time.perf_counter()
+    timeInvoke = timeEnd - timeStart
+    nthUtility.log_prompt_perf(userInput, perfLog, cb, timeInvoke)
+    #return response
+    responseString = response['answer']
+    if IncludeSource:
+        for doc in response['context']:
+            responseString += '\n\n'
+            responseString += doc.metadata['source']
+            responseString += ' Page '
+            responseString += str(doc.metadata['page'] + 1)
+    return responseString
+
+def get_no_rag_response(userInput):
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful ChatBot {context}"),
+        MessagesPlaceholder(variable_name="chatHistory"),
+        ("user", "{userInput}"),
+    ])
+    timeStart = time.perf_counter()
+    stuffDocumentsChain = create_stuff_documents_chain(llm,prompt)
+    with get_openai_callback() as cb:
+        response = stuffDocumentsChain.invoke({
+            "chatHistory": getattr(st.session_state, msgHistory),
+            "userInput": userInput,
+            "context": "",
+            }) #.content
+    timeEnd = time.perf_counter()
+    timeInvoke = timeEnd - timeStart
+    nthUtility.log_prompt_perf(userInput, perfLog, cb, timeInvoke)
+    if IncludeSource:
+        response += '\n\n'
+        response += 'No Sources... Not using Knowledgebase'
+
+    return response
+
+def generate_response(userInput):
+    nthUtility.log_prompt(userInput, logFile)
+    with st.chat_message('human'):
+        st.markdown(userInput)
+    getattr(st.session_state, msgHistory).append({"role": "human", "content": userInput})
+    with st.chat_message('assistant'):
+        if UseKnowledge:
+            response = get_response(userInput)
+        else:
+            response = get_no_rag_response(userInput)
+        st.write(response)
+    getattr(st.session_state, msgHistory).append({"role": "assistant", "content": response})
+
+#-------------------------------------------------------------
+# Streamlit Stuff
+
+st.set_page_config(page_title=webTitle)
+st.image(logo, width=200)
+st.divider()
+
+#tab1, tab2, tab3 = st.tabs(["Chat Interface", "Knowledgebase", "PDFs"])
+tab1, tab2, = st.tabs(["Chat Interface", "Knowledgebase"])
+
+# Sidebar
+UseKnowledge = st.sidebar.toggle("Use Knowledgebase")
+IncludeSource = st.sidebar.toggle("Cite Sources")
+
+# Session State
+if str(vsName) not in st.session_state:
+        if len(os.listdir(myDocs)) >= 1:
+            setattr(st.session_state, vsName, get_vectorstore())
+        else:
+            st.write("It looks like there are no files in your knowledgebase. Please upload some PDFs before proceeding.")
+            
+if str(msgHistory) not in st.session_state:
+    setattr(st.session_state, msgHistory, [])
+
+# Conversation
+with tab1:
+    for message in getattr(st.session_state, msgHistory):
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+        
+    # Display new Q&A    
+    userInput = st.chat_input("Ask your question...")
+    if userInput != None and userInput != "":
+        generate_response(userInput)
+
+with tab2:
+    st.subheader("List of Files in Knowledgebase:")
+    for file in os.listdir(myDocs):
+        st.markdown(file)
+    st.divider()
+
+    uploaded_files = st.file_uploader(
+        "Upload a file to the KnowledgeBase:", 
+        type=['pdf'],
+        accept_multiple_files=True,
+        label_visibility="collapsed")
+        
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            #st.success(f"File {uploaded_file.name} uploaded successfully!")
+            with open(os.path.join(myDocs, uploaded_file.name), "wb") as f:
+                f.write(uploaded_file.read())
+        regen_vectorstore()
+        st.success("knowledgebase updated")
